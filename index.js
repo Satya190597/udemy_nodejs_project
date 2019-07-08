4f7453ec7c33eb1f5952d5cdd8e12168f0a72545
satya190597 <webprakashsatya@gmail.com>
2019-07-08 18:47:06 +0530

    Added Basic Layout layout.css & Served Static Files

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const rootDirectory = require('./util/path')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/admin',admin)
 app.use(shop)
