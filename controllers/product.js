6adbf51abf2a79052802086d21a5c127b3c9fbd1
satya190597 <webprakashsatya@gmail.com>
2019-07-10 17:47:27 +0530

    Rearranging The View

--- controllers/product.js
+++ controllers/product.js
@@ -2,7 +2,7 @@ const Product = require('../models/product')
 
 exports.getAddProduct = (request,response) => {
     // response.status(200).sendFile(path.join(rootDirectory,'views','admin-product.html'))
-    response.status(200).render('admin-product',{title:'Add Product'})
+    response.status(200).render('admin/admin-product',{title:'Add Product'})
 }
 
 exports.postAddProduct = (request,response) => {
@@ -14,7 +14,7 @@ exports.postAddProduct = (request,response) => {
 exports.getAllProducts = (request,response) => {
     // response.sendFile(path.join(rootDirectory,'views','shop.html'))
     Product.fetchAll((products) => {
-        response.status(200).render('shop',{products:products,title:'Shop'})
+        response.status(200).render('shop/product-list',{products:products,title:'Shop'})
     })
     
 }
