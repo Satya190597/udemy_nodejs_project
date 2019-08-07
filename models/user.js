f350444825dd132120fc6283f3760a2761f76fac
Satya190597 <webprakashsatya@gmail.com>
2019-08-08 02:53:19 +0530

    Add-To-Cart with mongoose

--- models/user.js
+++ models/user.js
@@ -24,6 +24,32 @@ const user = new Schema({
         }]
     }
 })
+/*
+    --- Custom Methods ---
+*/
+user.methods.addToCart = function (product){
+    /*
+        1. Get user cart information
+        2. Check If Product Exist Or Not
+        3. If Exist Increment The Quantity
+        4. If Not Add The New Element
+    */    
+   console.log(user)
+   console.log(this.cart)
+   const userCartItems = [...this.cart.items]
+   const productIndex = userCartItems.findIndex( items => { return items.productId.toString() === product})
+   if(productIndex>-1)
+   {
+       userCartItems[productIndex].quantity = userCartItems[productIndex].quantity + 1;
+   }
+   else
+   {
+       userCartItems.push({productId:product,quantity:1})
+   }
+   this.cart.items = userCartItems
+   return this.save()
+}
+
 module.exports = mongoose.model('User',user)
 // const mongodb = require('mongodb')
 // const db = require('../util/datbase')
--- routes/user.js
+++ routes/user.js
@@ -3,55 +3,54 @@ const router = express.Router()
 
 const User = require('../models/user')
 
-router.post('/create-user',(request,response)=>{
-    const user = new User(request.body.username,request.body.email)
-    user.save((user)=>{
-        console.log(`User created successfully ${user}`)
-    })
-})
-/*
---- Add Order ---
-*/
-router.get('/user/add-order',(request,response) => {
-    const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
-    user.addOrders().then((result) => {
-        response.status(200).json(result)
-    })    
-})
+// router.post('/create-user',(request,response)=>{
+//     const user = new User(request.body.username,request.body.email)
+//     user.save((user)=>{
+//         console.log(`User created successfully ${user}`)
+//     })
+// })
+// /*
+// --- Add Order ---
+// */
+// router.get('/user/add-order',(request,response) => {
+//     const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
+//     user.addOrders().then((result) => {
+//         response.status(200).json(result)
+//     })    
+// })
 
-/*
---- Get Cart Item Rouet ---
-*/
-router.get('/user/get-cart',(request,response) => {
-    const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
-    user.getCart().then((cart) => {
-        response.status(200).json(cart)
-    })    
-})
+// /*
+// --- Get Cart Item Rouet ---
+// */
+// router.get('/user/get-cart',(request,response) => {
+//     const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
+//     user.getCart().then((cart) => {
+//         response.status(200).json(cart)
+//     })    
+// })
 
-/*
---- Delete Cart Item ---
-*/
-router.post('/user/delete-cart',(request,response) => {
-    const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
-    user.deleteCartItems(request.body.id).then((data) => {
-        response.status(200).json(data)
-    })
-})
+// /*
+// --- Delete Cart Item ---
+// */
+// router.post('/user/delete-cart',(request,response) => {
+//     const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
+//     user.deleteCartItems(request.body.id).then((data) => {
+//         response.status(200).json(data)
+//     })
+// })
 
-router.get('/user/:userId',(request,response)=>{
-    User.findById(request.params.userId)
-    .then((result)=>{
-        console.log('>>>> result' + result)
-    })
-})
+// router.get('/user/:userId',(request,response)=>{
+//     User.findById(request.params.userId)
+//     .then((result)=>{
+//         console.log('>>>> result' + result)
+//     })
+// })
 
 /*
 ---- Add To Cart Feature ----
 */
 router.post('/user/add-to-cart',(request,response) => {
-    const user = new User(request.user.name,request.user.email,request.user.cart,request.user._id)
-    user.addToCart(request.body.id)
+    request.user.addToCart(request.body.id)
     response.render('/products')
 })
 
